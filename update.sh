414c194132aebfb31e97a430ba6689785a4e5397
valetzx <45054400+valetzx@users.noreply.github.com>
2022-04-30 16:31:20 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #此脚本为手动更新，请关闭程序后使用！
 cd uptime-kuma
 git fetch --all
-git checkout 1.15.0 --force
+git checkout 1.15.1 --force
 #在replit shell中输入 sh update.sh 回车 以更新
 npm ci --production
 npm run download-dist
